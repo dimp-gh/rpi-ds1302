1655c2b091d2ab73243d252a200fa773bf94ddb5
Dmitry Andreevich <dmand@yandex.ru>
2014-01-15 14:00:19 +0300

    Oops, this doesn't meant to be here

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
 
-c_nviro = Extension('ds1302',
-					sources = ['python.c'],
-					libraries = ['wiringPi', 'm'],
-					extra_compile_args = ['-std=c99'],
-				)
+ds1302 = Extension('ds1302',
+				   sources = ['python.c'],
+				   libraries = ['wiringPi', 'm'],
+				   extra_compile_args = ['-std=c99'],
+			   )
 
 setup(name='rpi_rtc',
 	  version="0.1",
 	  description='Python library for handling DS1302 RTC with Raspberry Pi',
-	  ext_modules=[c_nviro],
+	  ext_modules=[ds1302],
 	  py_modules=['rpi_time', 'hwclock'],
-	  )
+)
